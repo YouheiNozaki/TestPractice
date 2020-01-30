1ac12ebf2446ce50742150ecd8d63b323705ed24
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-01-30 21:42:59 +0900

    congrats component

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-export default () => {
-  return <div></div>;
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @params {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if 'success` props)
+ */
+export default props => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
 };
