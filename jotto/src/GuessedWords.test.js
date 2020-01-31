c9e9d8cc15ab00237ce2bd865acee885122b8346
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-01-31 23:36:20 +0900

    Start tests for GuessWords

---
+++ jotto/src/GuessedWords.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps } from '../test/testUtils';
+import GuessedWords from './GuessedWords';
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
+};
+
+/**
+ * Factory function to create a ShallowWrapper for the App component.
+ * @function setup
+ * @param {object} props - component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const setUp = (props = {}) => {
+  const setUpProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setUpProps} />);
+};
+
+test('does not throw warning with expected props', () => {
+  checkProps(GuessedWords, defaultProps);
+});
+
+describe('if there are no words guessed', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setUp({ guessedWords: [] });
+  });
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+  test('renders instructions to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions');
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+describe('if there are words guessed', () => {});
