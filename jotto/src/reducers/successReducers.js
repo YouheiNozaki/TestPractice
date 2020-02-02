001b2dd67d179847cd3ba8e823095dd9b1f90dbd
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-02-02 19:06:50 +0900

    setting redux

---
+++ jotto/src/reducers/successReducers.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words
+ * @param {object} action -action tobe reduced
+ * @returns {boolean} - new success state
+ */
+export default (state, action) => {
+  return null;
+};
