ec1ff31a8a5911d7160f8abf0fc163f40047a9b4
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-02-08 14:34:42 +0900

    first hooks test

--- jotto-hooks/src/Input.js
+++ jotto-hooks/src/Input.js
@@ -2,7 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Input({ secretWord }) {
-  return <div data-test="component-input"></div>;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          value={currentGuess}
+          onChange={event => setCurrentGuess(event.target.value)}
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          onClick={evt => {
+            evt.preventDefault();
+            setCurrentGuess('');
+          }}
+          className="btn btn-promary mb-2"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 Input.propTypes = {
